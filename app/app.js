1d8b14aad854e33126d9f1801a9cf9dbecdacdf1
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-04-03 16:14:00 +0100

    added basic routing

--- app/app.js
+++ app/app.js
@@ -5,6 +5,13 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
+    res.send(`
+        <h1>Welcome</h1>
+        <p>Roux Academy Meetups put together artists from all walks of life</p>
+    `);
+});
+
+app.get('/speakers', function(req, res) {
     var info = '';
     dataFile.speakers.forEach(function(item) {
         info += `
@@ -17,6 +24,15 @@ app.get('/', function(req, res) {
     res.send(`
         <h1>Roux Academy Meetups</h1>
         ${info}
+    `);
+});
+
+app.get('/speakers/:speakerid', function(req, res) {
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(`
+        <h1>${speaker.title}</h1>
+        <h2>with ${speaker.name}</h2>
+        <p>${speaker.summary}</p>
     `);
 });
 
