2e0bc456aa88bc842a146b09276a516e5aaeb349
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-04-09 10:51:22 +0100

    added live chat functionality

--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -1,26 +1,40 @@
-var chatForm = document.forms.chatForm;
+var socket = io();
+var chatUsername = document.querySelector('#chat-username');
+var chatMessage = document.querySelector('#chat-message');
 
-if (chatForm) {
-    var chatUsername = document.querySelector('#chat-username');
-    var chatMessage = document.querySelector('#chat-message');
 
-    chatForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-        showMessage({
-            username: chatUsername.value,
-            message: chatMessage.value,
-        });
-        chatMessage.value='';
-        chatMessage.focus();
-    });
+socket.on('connect', function() {
+    var chatForm = document.forms.chatForm;
+    if (chatForm) {
+        chatForm.addEventListener('submit', function(e) {
+            e.preventDefault();
 
-}
+            socket.emit('postMessage', {
+                username: chatUsername.value,
+                message: chatMessage.value,
+            });
+
+            chatMessage.value='';
+            chatMessage.focus();
+        }); // chatform event
+
+        socket.on('updateMessages', function(data) {
+            showMessage(data);
+        }); // updateMessages
+    } // chatform
+}); // socket
 
 function showMessage(data) {
     var chatDisplay = document.querySelector('.chat-display');
     var newMessage = document.createElement('p');
-    newMessage.className = 'bg-success chat-text';
-    newMessage.innerHTML = '<strong>' + data.username + '</strong>:' + data.message;
+
+    if (chatUsername.value == data.username) {
+        newMessage.className = 'bg-success chat-text';
+    } else {
+        newMessage.className = 'bg-info text-warning chat-text';
+    }
+    
+    newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
 
     // basic DOM management, have to insert as the first in an element
     chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
